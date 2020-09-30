a29c5507d2cb50548b9f442824f611609a8f5d9c
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2020-10-01 02:10:28 +0530

    part4 Django tutorial

--- Building_Web_applicaitons_in_django/mysite/polls/urls.py
+++ Building_Web_applicaitons_in_django/mysite/polls/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import views
-app_name = 'polls'
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('owner', views.owner, name='owner'),
-    path('<int:pk>/', views.detail, name='detail'),
-    path('<int:pk>/results/', views.results, name='results'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
