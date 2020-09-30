5b24e7abef5d11ba6ace460a557c770a0776b48a
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2020-10-01 01:12:21 +0530

    Part2 Django tutorial

--- Building_Web_applicaitons_in_django/mysite/polls/admin.py
+++ Building_Web_applicaitons_in_django/mysite/polls/admin.py
@@ -1,3 +1,8 @@
+
 from django.contrib import admin
 
+from .models import Question,Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
 # Register your models here.
--- Building_Web_applicaitons_in_django/mysite/polls/views.py
+++ Building_Web_applicaitons_in_django/mysite/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. 596059d2 is the polls index.")
+
 
 # Create your views here.
